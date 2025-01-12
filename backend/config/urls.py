7541a4c7f76164e0add53174c09d037f8e5e7060
Ryota Shiba <s.ryota2416hatmp@gmail.com>
2025-01-12 19:20:20 +0900

    Overwrite repository with local files

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+# ルート (`/`) にアクセスしたときのビュー関数を定義
+def home_view(request):
+    return JsonResponse({"message": "Welcome to the API"}, status=200)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('auth/', include('customauth.urls')),
-    path('products/', include('products.urls')),
+    path("admin/", admin.site.urls),
+    path("auth/", include("customauth.urls")),
+    path("products/", include("products.urls")),
+    path("", home_view),  # ルート (`/`) にアクセスしたときのレスポンス
 ]
